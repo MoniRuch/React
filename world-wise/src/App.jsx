654c62656be30a168ed27cc9b886191daf115514
monica <sharmamonica0803@gmail.com>
2025-08-22 19:12:58 +0100

    Lazy loaded Pages and Suspense Fallback

--- world-wise/src/App.jsx
+++ world-wise/src/App.jsx
@@ -1,12 +1,6 @@
 import './App.css'
 import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 
-import Homepage from "./pages/Homepage.jsx";
-import Product from "./pages/Product.jsx";
-import Pricing from "./pages/Pricing.jsx";
-import PageNotFound from "./pages/PageNotFound.jsx";
-import Login from "./pages/Login.jsx";
-import AppLayout from "./pages/AppLayout.jsx";
 import CityList from "./components/CityList.jsx";
 import CountryList from "./components/CountryList.jsx";
 import City from "./components/City.jsx";
@@ -14,10 +8,28 @@ import Form from "./components/Form.jsx";
 import {CitiesProvider} from "./contexts/CitiesContext.jsx";
 import {AuthProvider} from "./contexts/FakeAuthContext.jsx";
 import ProtectedRoute from "./pages/ProtectedRoute.jsx";
+import {lazy, Suspense} from "react";
+import SpinnerFullPage from "./components/SpinnerFullPage.jsx";
+
+// import Homepage from "./pages/Homepage.jsx";
+// import Product from "./pages/Product.jsx";
+// import Pricing from "./pages/Pricing.jsx";
+// import PageNotFound from "./pages/PageNotFound.jsx";
+// import Login from "./pages/Login.jsx";
+// import AppLayout from "./pages/AppLayout.jsx";
+
+const Homepage = lazy(() => import("./pages/Homepage.jsx"));
+const Product = lazy(() => import("./pages/Product.jsx"));
+const Pricing = lazy(() => import("./pages/Pricing.jsx"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound.jsx"));
+const Login = lazy(() => import("./pages/Login.jsx"));
+const AppLayout = lazy(() => import("./pages/AppLayout.jsx"));
 
 function App() {
-  return (<AuthProvider><CitiesProvider>
+  return (<AuthProvider>
+        <CitiesProvider>
     <BrowserRouter>
+      <Suspense fallback={<SpinnerFullPage />}>
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="product" element={<Product />} />
@@ -34,6 +46,7 @@ function App() {
         </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
+      </Suspense>
     </BrowserRouter>
       </CitiesProvider>
       </AuthProvider>
