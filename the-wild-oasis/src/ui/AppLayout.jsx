c7b15e28952660eb86bf12f44d8b1f377a395f98
monica <sharmamonica0803@gmail.com>
2025-09-20 00:02:08 +0100

    Initial Commit: Starting with Styled Components

---
+++ the-wild-oasis/src/ui/AppLayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Outlet} from "react-router-dom";
+
+function AppLayout() {
+    return (<>
+        <div>AppLayout</div>
+        <Outlet/>
+    </>);
+}
+
+export default AppLayout;
