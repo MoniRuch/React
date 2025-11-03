b0fa827d68183e2adaf27495cea5d0e7c3726731
monica <sharmamonica0803@gmail.com>
2025-11-03 23:05:17 +0000

    Delete Confirmation & Reusable Compound Table

--- the-wild-oasis/src/features/cabins/CabinRow.jsx
+++ the-wild-oasis/src/features/cabins/CabinRow.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import React, {useState} from 'react';
+import React from 'react';
 
 import {formatCurrency} from "../../utils/helpers.js";
 import CreateCabinForm from "./CreateCabinForm.jsx";
@@ -7,18 +7,21 @@ import useDeleteCabin from "./useDeleteCabin.js";
 import {HiSquare2Stack} from "react-icons/hi2";
 import {HiPencil, HiTrash} from "react-icons/hi";
 import useCreateCabin from "./useCreateCabin.js";
+import Modal from "../../ui/Modal.jsx";
+import ConfirmDelete from "../../ui/ConfirmDelete.jsx";
+import Table from "../../ui/Table.jsx";
 
-const TableRow = styled.div`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-  column-gap: 2.4rem;
-  align-items: center;
-  padding: 1.4rem 2.4rem;
-
-  &:not(:last-child) {
-    border-bottom: 1px solid var(--color-grey-100);
-  }
-`;
+// const TableRow = styled.div`
+//   display: grid;
+//   grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
+//   column-gap: 2.4rem;
+//   align-items: center;
+//   padding: 1.4rem 2.4rem;
+//
+//   &:not(:last-child) {
+//     border-bottom: 1px solid var(--color-grey-100);
+//   }
+// `;
 
 const Img = styled.img`
   display: block;
@@ -49,7 +52,6 @@ const Discount = styled.div`
 
 function CabinRow({cabin}) {
     const {id: cabinId, image, name, maxCapacity, regularPrice, discount, description} = cabin;
-    const [showForm, setShowForm]  = useState(false);
 
    const {isDeleting, deleteCabin} = useDeleteCabin();
    const {isCreating, createCabin} = useCreateCabin();
@@ -65,8 +67,8 @@ function CabinRow({cabin}) {
         });
     }
    
-    return (<>
-        <TableRow role="row">
+    return (
+        <Table.Row>
             <Img src={image}/>
             <Cabin>{name}</Cabin>
             <div>Fits up to {maxCapacity} guests</div>
@@ -74,12 +76,23 @@ function CabinRow({cabin}) {
             {discount ? <Discount>{formatCurrency(discount)}</Discount> : <span>&mdash;</span>}
             <div>
                 <button disabled={isCreating} onClick={handleDuplicate}><HiSquare2Stack/></button>
-                <button onClick={() => setShowForm(show => !show)}><HiPencil /></button>
-                <button onClick={() => deleteCabin(cabinId)} disabled={isDeleting}><HiTrash /></button>
+                <Modal>
+                    <Modal.Open opens="edit">
+                        <button><HiPencil /></button>
+                    </Modal.Open>
+                    <Modal.Window name="edit">
+                        <CreateCabinForm cabinToEdit={cabin} />
+                    </Modal.Window>
+
+                    <Modal.Open opens="delete">
+                        <button><HiTrash /></button>
+                    </Modal.Open>
+                    <Modal.Window name="delete">
+                        <ConfirmDelete resourceName="cabins" disabled={isDeleting} onConfirm={() => deleteCabin(cabinId)}/>
+                    </Modal.Window>
+                </Modal>
             </div>
-        </TableRow>
-    {showForm && <CreateCabinForm cabinToEdit={cabin} />}
-    </>);
+        </Table.Row>);
 }
 
 export default CabinRow;
