2f0e698aca8d269ef132232d9b02cd35f49807b7
monica <sharmamonica0803@gmail.com>
2025-11-07 22:39:37 +0000

    Filter and Sort Bookings table

--- the-wild-oasis/src/features/bookings/useBookings.js
+++ the-wild-oasis/src/features/bookings/useBookings.js
@@ -1,10 +1,22 @@
 import {useQuery} from "@tanstack/react-query";
 import {getBookings} from "../../services/apiBookings.js";
+import {useSearchParams} from "react-router-dom";
 
 export default function useBookings(){
+    const [searchParams] = useSearchParams();
+    
+    //Filter
+    const filterValue = searchParams.get('status');
+    const filter = !filterValue || filterValue === 'all' ? null : {field : "status", value: filterValue}
+    
+    //Sort
+    const sortByValue = searchParams.get('sortBy') || 'startDate-desc';
+    const [field, direction] = sortByValue.split('-');
+    const sortBy = {field, direction}
+
     const {isLoading, data: bookings, error} = useQuery({
-        queryKey: ['booking'],
-        queryFn: getBookings
+        queryKey: ['booking', filter, sortBy],
+        queryFn: () => getBookings({filter, sortBy})
     })
     
     return {isLoading, bookings, error}
