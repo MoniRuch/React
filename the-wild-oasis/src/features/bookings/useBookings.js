882df73e8851123d56926039095bb8914fb2a234
monica <sharmamonica0803@gmail.com>
2025-11-10 22:50:21 +0000

    Pagination and Pre-fetching the Bookings Table

--- the-wild-oasis/src/features/bookings/useBookings.js
+++ the-wild-oasis/src/features/bookings/useBookings.js
@@ -1,8 +1,10 @@
-import {useQuery} from "@tanstack/react-query";
+import {useQuery, useQueryClient} from "@tanstack/react-query";
 import {getBookings} from "../../services/apiBookings.js";
 import {useSearchParams} from "react-router-dom";
+import {PAGE_SIZE} from "../../utils/constants.js";
 
 export default function useBookings(){
+    const queryClient = useQueryClient();
     const [searchParams] = useSearchParams();
     
     //Filter
@@ -13,11 +15,29 @@ export default function useBookings(){
     const sortByValue = searchParams.get('sortBy') || 'startDate-desc';
     const [field, direction] = sortByValue.split('-');
     const sortBy = {field, direction}
-
-    const {isLoading, data: bookings, error} = useQuery({
-        queryKey: ['booking', filter, sortBy],
-        queryFn: () => getBookings({filter, sortBy})
-    })
     
-    return {isLoading, bookings, error}
+    //Pagination
+    const page = !searchParams.get('page') ? 1 : +searchParams.get('page');
+
+    const {isLoading, data: {data: bookings, count} = {}, error} = useQuery({
+        queryKey: ['booking', filter, sortBy, page],
+        queryFn: () => getBookings({filter, sortBy, page})
+    })
+
+    //Pre-fetching
+    const pageCount = Math.ceil(count/PAGE_SIZE);
+    if(page < pageCount){
+        queryClient.prefetchQuery({
+            queryKey: ['booking', filter, sortBy, page + 1],
+            queryFn: () => getBookings({filter, sortBy, page: page + 1})
+        })
+    }
+    if(page > 1){
+        queryClient.prefetchQuery({
+            queryKey: ['booking', filter, sortBy, page - 1],
+            queryFn: () => getBookings({filter, sortBy, page: page - 1})
+        })
+    }
+    
+    return {isLoading, bookings, error, count}
 }
--- the-wild-oasis/src/services/apiBookings.js
+++ the-wild-oasis/src/services/apiBookings.js
@@ -1,10 +1,11 @@
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
+import {PAGE_SIZE} from "../utils/constants.js";
 
-export async function getBookings({filter, sortBy}) {
+export async function getBookings({filter, sortBy, page}) {
   let query = supabase
       .from('bookings')
-      .select('id,created_at,startDate,endDate,numNights,numGuests,totalPrice,status, cabins(name), guests(fullName, email)');
+      .select('id,created_at,startDate,endDate,numNights,numGuests,totalPrice,status, cabins(name), guests(fullName, email)', {count: 'exact'});
 
   //Filter
   if(filter) 
@@ -14,13 +15,21 @@ export async function getBookings({filter, sortBy}) {
   if(sortBy)
     query = query.order(sortBy.field, {ascending: sortBy.direction === 'asc'})
   
-  const { data, error } = await query;
+  //Page
+  if(page){
+    const from = (page - 1) * PAGE_SIZE;
+    const to = from + PAGE_SIZE - 1;
+    query = query.range(from, to)
+  }
+    
+  
+  const { data, error, count } = await query;
   if(error){
     console.error(error);
     throw new Error('There is a problem getting booking')
   }
 
-  return data;
+  return {data, count};
 }
 
 export async function getBooking(id) {
