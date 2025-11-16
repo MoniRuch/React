2ea570e7bd574184db0b68ea747d2c3cea0d7a73
monica <sharmamonica0803@gmail.com>
2025-11-16 22:49:32 +0000

    User Signup and Update User Detail & Password

--- the-wild-oasis/src/ui/Header.jsx
+++ the-wild-oasis/src/ui/Header.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import styled from "styled-components";
-import Logout from "../features/authentication/Logout.jsx";
+import HeaderMenu from "./HeaderMenu.jsx";
+import UserAvatar from "../features/authentication/UserAvatar.jsx";
 
 const StyledHeader = styled.header`
     border-bottom: 1px solid var(--color-grey-100);
     background: var(--color-grey-0);
     padding: 1.2rem 4.8rem;
+    display: flex;
+    gap: 2.6rem;
+    align-items: center;
+    justify-content: flex-end;
 `;
 function Header() {
     return (
         <StyledHeader>
-            <Logout />
+            <UserAvatar />
+            <HeaderMenu />
         </StyledHeader>
     );
 }
