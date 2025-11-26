bedc7d794d83b6ec5b567553ecbee6e7b5aac46e
monica <sharmamonica0803@gmail.com>
2025-11-26 09:20:39 +0000

    The Wild Oasis, Client website based on Next.js

---
+++ app/layout.js
@@ -0,0 +1,32 @@
+import "@/app/_styles/globals.css";
+import { Josefin_Sans } from "next/font/google";
+import Header from "./_components/Header";
+
+const josefin = Josefin_Sans({
+    subsets: ["latin"],
+    display: "swap",
+});
+
+export const metadata = {
+    title: {
+        template: "%s / The Wild Oasis",
+        default: "Welcome / The Wild Oasis",
+    },
+    description:
+        "Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountains and dark forests",
+};
+
+export default function RootLayout({children}){
+    return (
+        <html lang="en">
+            <body className={`${josefin.className} bg-primary-950 text-accent-50 min-h-screen flex flex-col`}>
+                <Header />
+                <div className="flex-1 px-8 py-12">
+                    <main className="mx-auto max-w-7xl">
+                        {children}
+                    </main>
+                </div>
+
+            </body>
+        </html>)
+}
